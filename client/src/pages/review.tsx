bf835fef4d8ce2fae2ebb4c6cd8e4582ad30c7c6
IBEJesusPrince <ibejesusprince@gmail.com>
2026-05-24 19:49:35 -0400

    Remove reschedule button from MISSED and SKIPPED sections

--- client/src/pages/review.tsx
+++ client/src/pages/review.tsx
@@ -866,15 +866,6 @@ export default function ReviewPage() {
                         >
                           <XCircle className="w-4 h-4" />
                         </button>
-                        {!external && (
-                          <button
-                            title="Reschedule"
-                            onClick={() => setRescheduleItem(item)}
-                            className={cn("p-1 rounded hover:bg-accent transition-colors", STATUS_CONFIG.rescheduled.className)}
-                          >
-                            <CalendarClock className="w-4 h-4" />
-                          </button>
-                        )}
                       </div>
                     </div>
                   );
@@ -978,15 +969,6 @@ export default function ReviewPage() {
                         >
                           <XCircle className="w-4 h-4" />
                         </button>
-                        {!external && (
-                          <button
-                            title="Reschedule"
-                            onClick={() => setRescheduleItem(item)}
-                            className={cn("p-1 rounded hover:bg-accent transition-colors", STATUS_CONFIG.rescheduled.className)}
-                          >
-                            <CalendarClock className="w-4 h-4" />
-                          </button>
-                        )}
                       </div>
                     </div>
                   );
